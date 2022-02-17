bef60a92b53e16c9a428694d59b8371be8d0f2fc
jaydubey25 <91167389+jaydubey25@users.noreply.github.com>
2022-02-17 16:45:03 +0530

    Create all data types.py

---
+++ all data types.py
@@ -0,0 +1,20 @@
+String1 = 'Welcome to the Alpha'
+print(String1) 
+
+num1=10
+print(num1)
+
+fnum=26.5
+print(fnum)
+
+c=4+2j
+print(c)
+
+l=[1,2,3,4,5]
+print(l)
+
+t=(1,2,3,4)
+print(t)
+
+d={1:"ID",2:"Name",3:"Dept"}
+print(d[1])
